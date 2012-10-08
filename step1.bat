1a1b26d9756cae8e18afea0a87b8a5a09eca63ab
Aygiz Kunafin <aigizk@gmail.com>
2012-10-08 11:57:42 +0600

    modified BAT file

--- step1.bat
+++ step1.bat
@@ -1,3 +1,5 @@
 start Platform.Node/bin/Debug/Platform.Node.exe
 @ping localhost -w 1000 -n 2 > nul
-start "" Platform.TestClient/bin/Debug/Platform.TestClient.exe 127.0.0.1 8080 WEFL
+start "" Platform.TestClient/bin/Debug/Platform.TestClient.exe 127.0.0.1 8080 USAGE
+@ping localhost -w 1000 -n 3 > nul
+taskkill /IM Platform.Node.exe
