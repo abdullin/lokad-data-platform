dd91b1e92519176c659470373fdc4597cba88c22
Aygiz Kunafin <aigizk@gmail.com>
2012-10-08 09:40:53 +0600

    add write processor command

---
+++ Platform.TestClient/Commands/WriteProccessor.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using Platform.Node;
+
+namespace Platform.TestClient.Commands
+{
+    public class WriteProccessor : ICommandProcessor
+    {
+        public string Key { get { return "WR"; } }
+        public string Usage { get { return "WR [<stream-id> <expected-version> <data>]"; } }
+        public bool Execute(CommandProcessorContext context, string[] args)
+        {
+            var eventStreamId = "default stream";
+            var expectedVersion = -1;
+            var data = "default-data";
+
+            if (args.Length > 0)
+            {
+                if (args.Length != 3)
+                {
+                    context.Log.Info("More arguments: {0}",args.Length);
+                    return false;
+                }
+
+                eventStreamId = args[0];
+                int.TryParse(args[1], out expectedVersion);
+                data = args[2];
+            }
+
+            context.IsAsync();
+
+            context.Client.JsonClient.Post<ClientDto.WriteEvent>("/stream", new ClientDto.WriteEvent()
+            {
+                Data = Encoding.UTF8.GetBytes(data),
+                Stream = eventStreamId,
+                ExpectedVersion = expectedVersion
+            });
+
+            context.Completed();
+            return true;
+        }
+    }
+}
