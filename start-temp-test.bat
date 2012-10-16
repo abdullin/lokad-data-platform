a8cb133f24d4b08f297c8137bae2834f09a65d48
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-10-16 18:42:36 +0600

    Lower server testing on WRFL

--- start-temp-test.bat
+++ start-temp-test.bat
@@ -1,2 +1,2 @@
-bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 5 1000
+bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 5 20
 bin\client\Platform.TestClient.exe -h 8085 SHUTDOWN
