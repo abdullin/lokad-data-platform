a29e84a1fc592a61f4dfbfba6f17b179e5e76323
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-10-16 19:18:30 +0600

    More test configs for WRFL

--- start-temp-test.bat
+++ start-temp-test.bat
@@ -1,2 +1,5 @@
 bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 5 20
+bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 10 10
+bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 2 50
+
 bin\client\Platform.TestClient.exe -h 8085 SHUTDOWN
