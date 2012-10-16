a531776d85490471022f1b328e97e7da2604cfca
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-10-16 18:11:57 +0600

    Toggle test data dirs

--- start-temp-test.bat
+++ start-temp-test.bat
@@ -1,2 +1,2 @@
-bin\client\Platform.TestClient.exe -h 8085 -t 30 WRFL 5 1000
+bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 30 WRFL 5 1000
 bin\client\Platform.TestClient.exe -h 8085 SHUTDOWN
