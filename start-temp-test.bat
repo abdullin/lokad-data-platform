4635be8f4c4ab5832df391b65dd2ebc9dde3b764
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-10-18 16:52:53 +0600

    write basic test in build

--- start-temp-test.bat
+++ start-temp-test.bat
@@ -1,5 +1,6 @@
 bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 5 20
 bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 10 10
 bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 WRFL 2 50
+bin\client\Platform.TestClient.exe -h 8085 -s "C:/LokadData/dp-test" -t 50 Basictest 10 10000 10 20
 
 bin\client\Platform.TestClient.exe -h 8085 SHUTDOWN
