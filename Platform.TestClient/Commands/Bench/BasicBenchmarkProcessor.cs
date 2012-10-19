bb0372a6434ad25510f2f3a808df4bc33f91ff49
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-10-19 21:15:17 +0600

    refactor benchmark

--- Platform.TestClient/Commands/Bench/BasicBenchmarkProcessor.cs
+++ Platform.TestClient/Commands/Bench/BasicBenchmarkProcessor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Platform.TestClient.Commands.Bench
@@ -8,37 +10,61 @@ namespace Platform.TestClient.Commands.Bench
         public string Key { get { return "BENCH1"; } }
         public string Usage { get { return "BENCH1"; } }
 
-        static string[] Args(string data = null)
+        
+        public sealed class BenchmarkTask
         {
-            if (string.IsNullOrWhiteSpace(data))
-                return new string[0];
-            return data.Split(' ');
+            public readonly ICommandProcessor Processor;
+            public readonly string Args;
+
+            public BenchmarkTask(ICommandProcessor processor, string args)
+            {
+                Processor = processor;
+                Args = args;
+            }
+
+            public string[] GetCommandArgs()
+            {
+                if (string.IsNullOrWhiteSpace(Args))
+                    return new string[0];
+                return Args.Split(' ','\t');
+            }
+        }
+
+        sealed class BenchmarkTaskList
+        {
+            public readonly IList<BenchmarkTask> Tasks = new List<BenchmarkTask>();
+
+            public void Add(ICommandProcessor processor, string args)
+            {
+                Tasks.Add(new BenchmarkTask(processor, args));
+            }
         }
 
         public bool Execute(CommandProcessorContext context, CancellationToken token, string[] args)
         {
+
+            var list = new BenchmarkTaskList();
+            list.Add(new ResetStoreProcessor(),"");
+            list.Add(new StartLocalServerProcessor(), "-k 300");
+            list.Add(new WriteEventsFloodProcessor(), "5 20 44");
+            list.Add(new WriteEventsFloodProcessor(), "10 10 44");
+            list.Add(new WriteEventsFloodProcessor(), "5 20 600");
+            list.Add(new WriteEventsFloodProcessor(), "10 10 600");
+            list.Add(new BasicTestProcessor(), "10 10000 10 20");
+            list.Add(new WriteBatchFloodProcessor(), "1 500000 5");
+            list.Add(new WriteBatchFloodProcessor(), "5 500000 5");
+            
             try
             {
-                var success = true;
-                success &= new ResetStoreProcessor().Execute(context, token, Args());
-                success &= new StartLocalServerProcessor().Execute(context, token, Args("-k 300"));
-                // we need to sleep a little bit to let server wire up
-                // TODO: setup proper ping inside server starter
-                token.WaitHandle.WaitOne(10000);
-
-                success &= new WriteEventsFloodProcessor().Execute(context, token, Args("5 20 44"));
-                success &= new WriteEventsFloodProcessor().Execute(context, token, Args("10 10 44"));
-
-                success &= new WriteEventsFloodProcessor().Execute(context, token, Args("5 20 600"));
-                success &= new WriteEventsFloodProcessor().Execute(context, token, Args("10 10 600"));
-                
-                
-                success &= new BasicTestProcessor().Execute(context, token, Args("10 10000 10 20"));
-
-                success &= new WriteBatchFloodProcessor().Execute(context, token, Args("1 500000 5"));
-                success &= new WriteBatchFloodProcessor().Execute(context, token, Args("5 500000 5"));
-                success &= new WriteBatchFloodProcessor().Execute(context, token, Args("10 500000 5"));
-                return success;
+                foreach (var task in list.Tasks)
+                {
+                    if (!task.Processor.Execute(context, token, task.GetCommandArgs()))
+                    {
+                        context.Log.Error("Failure while running {0} {1}", task.Processor.Key, task.Args);
+                        return false;
+                    }
+                }
+                return true;
             }
             catch (Exception ex)
             {
