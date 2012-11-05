533d6bfacfb83495c63149b9c9f3e1216d324519
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-11-05 20:23:41 +0600

    clean up write processor

--- Platform.TestClient/Commands/WriteProccessor.cs
+++ Platform.TestClient/Commands/WriteProccessor.cs
@@ -6,7 +6,7 @@ namespace Platform.TestClient.Commands
     public class WriteProccessor : ICommandProcessor
     {
         public string Key { get { return "WR"; } }
-        public string Usage { get { return @"WR [<stream-id> <expected-version> <data>]
+        public string Usage { get { return @"WR [<stream-id> <data>]
     Writes a single event to the event store"; } }
         public bool Execute(CommandProcessorContext context, CancellationToken token, string[] args)
         {
@@ -15,16 +15,15 @@ namespace Platform.TestClient.Commands
 
             if (args.Length > 0)
             {
-                if (args.Length != 3)
+                if (args.Length != 2)
                 {
-                    context.Log.Info("More arguments: {0}",args.Length);
+                    context.Log.Info("Expected zero or {0} arguments",args.Length);
                     return false;
                 }
 
                 eventStreamId = args[0];
-                int expectedVersion;
-                int.TryParse(args[1], out expectedVersion);
-                data = args[2];
+
+                data = args[1];
             }
             context.Client.Streams.WriteEvent(eventStreamId, Encoding.UTF8.GetBytes(data));
             return true;
