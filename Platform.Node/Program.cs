95150c30f0ab419e745dc21902f8cbeda09795b5
Aygiz Kunafin <aigizk@gmail.com>
2012-12-27 13:59:17 +0600

    server runs as a windows service [touch: 72]

--- Platform.Node/Program.cs
+++ Platform.Node/Program.cs
@@ -1,115 +1,19 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using Platform.Messages;
-using Platform.Node.Services.ServerApi;
-using Platform.Node.Services.Storage;
-using Platform.Node.Services.Timer;
+﻿using Topshelf;
 
 namespace Platform.Node
 {
     class Program
     {
-        public static readonly ILogger Log = LogManager.GetLoggerFor<Program>();
-        static readonly ManualResetEventSlim ExitWait = new ManualResetEventSlim(false);
-        static void Main(string[] args)
+        static void Main()
         {
-            // This is extremely important to enable high throughput 
-            // of individual messages
-            ServicePointManager.UseNagleAlgorithm = false;
-            ServicePointManager.DefaultConnectionLimit = 48;
-
-            var options = new NodeOptions();
-
-            var lineAsString = string.Join(" ", args);
-
-            if (lineAsString == "/?" || lineAsString == "--help")
+            HostFactory.Run(x =>
             {
-                Console.WriteLine("Usage");
-                Console.WriteLine(options.GetUsage());
-                return;
-            }
+                x.SetDescription("Lokad-DataPlatform");
+                x.SetDisplayName("Lokad-DataPlatform");
+                x.SetServiceName("Lokad-DataPlatform");
 
-            if (!CommandLine.CommandLineParser.Default.ParseArguments(args, options))
-            {
-                return;
-            }
-
-            var list = string.Join(Environment.NewLine,
-                options.GetPairs().Select(p => string.Format("{0} : {1}", p.Key, p.Value)));
-
-            Log.Info(list);
-
-            var bus = new InMemoryBus("OutputBus");
-            var controller = new NodeController(bus);
-            var mainQueue = new QueuedHandler(controller, "Main Queue");
-            controller.SetMainQueue(mainQueue);
-            Application.Start(ExitAction);
-            
-            var port = options.HttpPort;
-
-            var http = new PlatformServerApiService(mainQueue, string.Format("http://*:{0}/", port));
-
-            bus.Subscribe<SystemMessage.Init>(http);
-            bus.Subscribe<SystemMessage.StartShutdown>(http);
-
-
-            var timer = new TimerService(new ThreadBasedScheduler(new RealTimeProvider()));
-            bus.Subscribe<TimerMessage.Schedule>(timer);
-
-            // switch, based on configuration
-            AzureStoreConfiguration azureConfig;
-            if (AzureStoreConfiguration.TryParse(options.StoreLocation, out azureConfig))
-            {
-                var storageService = new AzureStorageService(azureConfig, mainQueue);
-                bus.Subscribe<ClientMessage.AppendEvents>(storageService);
-                bus.Subscribe<SystemMessage.Init>(storageService);
-                bus.Subscribe<ClientMessage.ImportEvents>(storageService);
-                bus.Subscribe<ClientMessage.RequestStoreReset>(storageService);
-            }
-            else
-            {
-                var storageService = new FileStorageService(options.StoreLocation, mainQueue);
-                bus.Subscribe<ClientMessage.AppendEvents>(storageService);
-                bus.Subscribe<SystemMessage.Init>(storageService);
-                bus.Subscribe<ClientMessage.ImportEvents>(storageService);
-                bus.Subscribe<ClientMessage.RequestStoreReset>(storageService);
-            }
-
-            
-            mainQueue.Start();
-
-            mainQueue.Enqueue(new SystemMessage.Init());
-            
-            if (options.KillSwitch > 0)
-            {
-                var seconds = TimeSpan.FromSeconds(options.KillSwitch);
-                mainQueue.Enqueue(
-                    TimerMessage.Schedule.Create(seconds, 
-                    new PublishEnvelope(mainQueue), 
-                    new ClientMessage.RequestShutdown()));
-            }
-            var interactiveMode = options.KillSwitch <= 0;
-
-            if (interactiveMode)
-            {
-                Console.Title = string.Format("Test server : {0} : {1}", options.HttpPort, options.StoreLocation);
-                Console.WriteLine("Starting everything. Press enter to initiate shutdown");
-                Console.ReadLine();
-                mainQueue.Enqueue(new ClientMessage.RequestShutdown());
-                Console.ReadLine();
-            }
-            else
-            {
-                ExitWait.Wait();
-            }
-        }
-
-        static void ExitAction(int i)
-        {
-            ExitWait.Set();
-            Environment.Exit(i);
+                x.Service(settings => new ServerNode());
+            });
         }
     }
 }
