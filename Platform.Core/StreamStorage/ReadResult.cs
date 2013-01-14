a1e50aff9b594b0eaf665099d14c654550555246
Joannes Vermorel <joannes.vermorel@lokad.com>
2013-01-14 15:31:08 +0100

    Cosmetic: comment added.

--- Platform.Core/StreamStorage/ReadResult.cs
+++ Platform.Core/StreamStorage/ReadResult.cs
@@ -2,9 +2,14 @@ using System.Collections.Generic;
 
 namespace Platform.StreamStorage
 {
+    /// <summary>
+    /// In order to reduce latency between reads, it's important to read multiple records
+    /// at once. This class represents the result of multi-record read operation.
+    /// </summary>
     public struct ReadResult
     {
         public readonly long NextOffset;
+
         public readonly ICollection<RetrievedDataRecord> Records;
 
         public ReadResult(long nextOffset, ICollection<RetrievedDataRecord> records) : this()
