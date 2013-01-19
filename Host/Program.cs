092fc19b2df34c31d3726395857970b11af9afdd
Aygiz Kunafin <aigizk@gmail.com>
2013-01-19 14:22:36 +0600

    rebuild when change AddIn files

--- Host/Program.cs
+++ Host/Program.cs
@@ -3,8 +3,10 @@ using System.AddIn.Hosting;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Platform;
 using Platform.MafHVA;
@@ -17,6 +19,11 @@ namespace Host
     {
         static IRawEventStoreClient _reader;
         static ViewClient _views;
+        static Dictionary<string, DateTime> _lastAddInChangeInfo;
+        static bool _addInIsChange;
+        static string _pipelineRootFolderPath;
+        static IEnumerable<AddInToken> _tokens;
+        static int _runingDllCount;
 
         static void Main(string[] args)
         {
@@ -24,35 +31,89 @@ namespace Host
             var storeConnection = ConfigurationManager.AppSettings["StoreConnection"];
             _reader = PlatformClient.ConnectToEventStore(storePath, "default", storeConnection);
             _views = PlatformClient.ConnectToViewStorage(storePath, "host-run-views");
+            // Assume that the current directory is the application folder,  
+            // and that it contains the pipeline folder structure. 
+            _pipelineRootFolderPath = Environment.CurrentDirectory + "\\Pipeline";
+            _tokens = new AddInToken[0];
+            _lastAddInChangeInfo = new Dictionary<string, DateTime>();
 
-            RebuildRun();
+            ThreadPool.QueueUserWorkItem(x =>
+            {
+                while (true)
+                {
+                    _addInIsChange = AddInIsChange(_pipelineRootFolderPath);
+                    if (_addInIsChange)
+                    {
+                        while (_runingDllCount > 0)
+                            Thread.Sleep(100);
+
+                        _addInIsChange = false;
+                        RebuildRun();
+                    }
+
+                    Thread.Sleep(5000);
+                }
+            }, Thread.CurrentThread);
+
+            Console.ReadLine();
         }
 
         static void RebuildRun()
         {
-            // Assume that the current directory is the application folder,  
-            // and that it contains the pipeline folder structure. 
-            var addInRoot = Environment.CurrentDirectory + "\\Pipeline";
+            _runingDllCount = 0;
 
             //Check to see if new add-ins have been installed.
-            AddInStore.Rebuild(addInRoot);
+            AddInStore.Rebuild(_pipelineRootFolderPath);
 
-            IEnumerable<AddInToken> tokens = GetLastVersionToken(addInRoot);
+            _tokens = GetLastVersionToken(_pipelineRootFolderPath);
 
-            bool changeDllFolders = false;
-
-            Parallel.ForEach(tokens, token =>
-                {
-                    var run = token.Activate<MafRun>(AddInSecurityLevel.FullTrust);
-
-                    while (!changeDllFolders)
+            foreach (AddInToken token in _tokens)
+            {
+                Task.Factory.StartNew(() =>
                     {
-                        Running(run);
-                    }
-                });
+                        Interlocked.Increment(ref _runingDllCount);
+                        var run = token.Activate<MafRun>(AddInSecurityLevel.FullTrust);
 
-            if (changeDllFolders)
-                RebuildRun();
+                        while (!_addInIsChange)
+                        {
+                            Running(run);
+                        }
+
+                        // shutdown the add-in when the IRun method finishes executing
+                        AddInController controller = AddInController.GetAddInController(run);
+                        controller.Shutdown();
+
+                        Interlocked.Decrement(ref _runingDllCount);
+                    },
+                    TaskCreationOptions.LongRunning | TaskCreationOptions.PreferFairness);
+            }
+        }
+
+        static bool AddInIsChange(string addInRoot)
+        {
+            bool addInChange = false;
+            var dllFiles = new DirectoryInfo(addInRoot).GetFiles("*.dll", SearchOption.AllDirectories);
+
+            List<string> oldFiles = _lastAddInChangeInfo.Select(x => x.Key).ToList();
+            foreach (var dllFile in dllFiles)
+            {
+                if (_lastAddInChangeInfo.ContainsKey(dllFile.FullName))
+                    oldFiles.Remove(dllFile.FullName);
+
+                if (!_lastAddInChangeInfo.ContainsKey(dllFile.FullName) ||
+                    _lastAddInChangeInfo[dllFile.FullName] != dllFile.LastWriteTimeUtc)
+                {
+                    addInChange = true;
+                    _lastAddInChangeInfo[dllFile.FullName] = dllFile.LastWriteTimeUtc;
+                }
+            }
+
+            if (oldFiles.Count > 0)
+                addInChange = true;
+            foreach (string oldFile in oldFiles)
+                _lastAddInChangeInfo.Remove(oldFile);
+
+            return addInChange;
         }
 
         static void Running(MafRun run)
