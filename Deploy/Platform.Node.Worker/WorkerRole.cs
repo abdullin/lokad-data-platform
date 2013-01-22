b1eccd4e19806ea0765c439d74b8a5dd221f46cb
Vsevolod Parfenov <vsevolodp@gmail.com>
2013-01-22 16:25:30 +0600

    More traces and class for writing traces into blob

--- Deploy/Platform.Node.Worker/WorkerRole.cs
+++ Deploy/Platform.Node.Worker/WorkerRole.cs
@@ -29,7 +29,6 @@ namespace Platform.Node.Worker
                 
 
                 //var endpointUrl = "http://" + endpoint + "/";
-                Trace.WriteLine("Listening on port" + endpoint.Port, "Information");
                 var options = new NodeOptions();
                 if (!CommandLineParser.Default.ParseArguments(param.Split(' '), options))
                     throw new Exception("Failed to parse: " + param);
@@ -37,6 +36,16 @@ namespace Platform.Node.Worker
                 options.HttpPort = endpoint.Port;
                 options.LocalHttpIp = endpoint.Address.ToString();
 
+                // Uncomment these lines and class BlobTraceListener to write traces into blob.
+                //var parts = options.StoreLocation.Split('|');
+                //var account = CloudStorageAccount.Parse(parts[0]);
+                //var client = account.CreateCloudBlobClient();
+                //var container = client.GetContainerReference(parts[1]);
+                //var blobListener = new BlobTraceListener(container.GetBlobReference("log.txt"));
+                //Trace.Listeners.Add(blobListener);
+
+                Trace.WriteLine("");
+                Trace.WriteLine(string.Format("Listening endpoint http://{0}:{1}/", endpoint.Address, endpoint.Port), "Information");
 
                 _entryPoint = NodeEntryPoint.StartWithOptions(options, i => RoleEnvironment.RequestRecycle());
             }
@@ -46,16 +55,22 @@ namespace Platform.Node.Worker
                 throw;
             }
 
-            return base.OnStart();
+            var result = base.OnStart();
+
+            Trace.WriteLine("OnStart:Exit");
+            return result;
         }
 
         public override void Run()
         {
+            Trace.WriteLine("Run:Enter");
             _entryPoint.WaitForServiceToExit();
+            Trace.WriteLine("Run:Exit");
         }
 
         public override void OnStop()
         {
+            Trace.WriteLine("OnStop:Enter");
             _entryPoint.RequestServiceStop();
             var finished = _entryPoint.WaitForServiceToExit(5000);
             
@@ -63,9 +78,9 @@ namespace Platform.Node.Worker
                 "OnStop:Worker role shutdown completed" : "OnStop:Forcing worker role shutdown",
                 "Information");
 
-            
-
             base.OnStop();
+
+            Trace.WriteLine("OnStop:Exit");
         }
 
         static void RoleEnvironmentChanging(object sender, RoleEnvironmentChangingEventArgs e)
@@ -78,4 +93,41 @@ namespace Platform.Node.Worker
             }
         }
     }
+
+    //public class BlobTraceListener : TraceListener
+    //{
+    //    readonly CloudBlob _blob;
+
+    //    public BlobTraceListener(CloudBlob blob)
+    //    {
+    //        _blob = blob;
+    //    }
+
+    //    [MethodImpl(MethodImplOptions.Synchronized)]
+    //    public override void Write(string message)
+    //    {
+    //        var log = string.Empty;
+    //        try
+    //        {
+    //            log = _blob.DownloadText();
+    //        }
+    //        // ReSharper disable EmptyGeneralCatchClause
+    //        catch {}
+    //        // ReSharper restore EmptyGeneralCatchClause
+
+    //        log = log + message;
+    //        try
+    //        {
+    //            _blob.UploadText(log);
+    //        }
+    //        // ReSharper disable EmptyGeneralCatchClause
+    //        catch { }
+    //        // ReSharper restore EmptyGeneralCatchClause
+    //    }
+
+    //    public override void WriteLine(string message)
+    //    {
+    //        Write(message + Environment.NewLine);
+    //    }
+    //}
 }
